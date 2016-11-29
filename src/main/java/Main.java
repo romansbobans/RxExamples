6aec75797a4c28fac3d60d3ca3d80a85568fe38f
romans <romans.stepanovs@gmail.com>
2016-11-29 19:52:13 +0200

    Removed unused comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,11 +10,5 @@ public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
         InputStream in = System.in;
         new ReactiveSum(in, "a:", "b:").combine(a -> a*3);
-        //a = 5; //a:5
-        //b = 5; //b:5
-        //a=7; //a:7
-
-        //new RxZip().fetchUserData().observeOn(Schedulers.io()).subscribe(v -> System.out.println(String.format("Finished execution on thread %s", Thread.currentThread().getName())));
-        //Thread.sleep(15000);
     }
 }
